a6b3dd0161cb31447bbf247bd0d31ce9b10a477d
Chickwolf <chickwolf@hotmail.com>
2018-04-30 08:27:55 +0100

    Add files via upload

---
+++ Rhonada_simulator.ino
@@ -0,0 +1,31 @@
+// Rhondda simultator
+// Atlas AV - V1.0
+
+  void extractorOn(){digitalWrite(2,HIGH);}
+  void extractorOff(){digitalWrite(2,LOW);}
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+}
+  String inBuffer = "";
+void loop() 
+{
+    while (Serial.available()){
+    char c = Serial.read();
+    inBuffer += c;
+    }
+      byte incomingByte = Serial.read();
+      if (inBuffer == "extractorOn")
+      {
+        extractorOn();
+        Serial.println("On");
+      }
+      else if (inBuffer == "extractorOff")
+      {
+        extractorOff();
+        Serial.println("Off");         
+      }
+  inBuffer = "";
+}
